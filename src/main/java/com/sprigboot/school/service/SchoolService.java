998ec239d4384a1da7b15b4cfe3134e4b262c8dc
maheshabeykoon <maheshpabeykoon@gmail.com>
2022-07-13 11:57:23 +0530

    Attach service layer to repository layer

--- src/main/java/com/sprigboot/school/service/SchoolService.java
+++ src/main/java/com/sprigboot/school/service/SchoolService.java
@@ -1,5 +1,15 @@
 package com.sprigboot.school.service;
 
+import org.springframework.stereotype.Service;
+
+import com.sprigboot.school.repository.SchoolRepository;
+
+@Service
 public class SchoolService {
 
+	private SchoolRepository schoolRepository;
+	
+	SchoolService(SchoolRepository schoolRepository){
+		this.schoolRepository= schoolRepository;
+	}
 }
