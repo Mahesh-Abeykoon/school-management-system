a1f49814a4a47769df8dbe593c20edca94ca87b1
maheshabeykoon <maheshpabeykoon@gmail.com>
2022-07-13 12:55:30 +0530

    Create School details post API

--- src/main/java/com/sprigboot/school/controller/SchoolController.java
+++ src/main/java/com/sprigboot/school/controller/SchoolController.java
@@ -1,12 +1,18 @@
 package com.sprigboot.school.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import com.sprigboot.school.model.School;
 import com.sprigboot.school.service.SchoolService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-
+@RestController
+@RequestMapping("/api/schools")
 
 public class SchoolController {
 
@@ -16,7 +22,12 @@ public class SchoolController {
 		this.schoolService=schoolService;
 	}
 	
-	
+	@PostMapping()
+	public School saveSchoolDetails (@RequestBody School school) {
+		return schoolService.saveSchool(school);
+		 
+		 
+	}
 	
 	
 	
--- src/main/java/com/sprigboot/school/service/SchoolService.java
+++ src/main/java/com/sprigboot/school/service/SchoolService.java
@@ -2,6 +2,7 @@ package com.sprigboot.school.service;
 
 import org.springframework.stereotype.Service;
 
+import com.sprigboot.school.model.School;
 import com.sprigboot.school.repository.SchoolRepository;
 
 @Service
@@ -11,5 +12,10 @@ public class SchoolService {
 	
 	SchoolService(SchoolRepository schoolRepository){
 		this.schoolRepository= schoolRepository;
+	}
+
+	public School saveSchool(School school) {
+		
+		return schoolRepository.save(school);
 	}
 }
