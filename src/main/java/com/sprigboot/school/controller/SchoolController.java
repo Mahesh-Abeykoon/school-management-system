78d3e0543a5f614d40a5edb6ca1b372c4288640a
maheshabeykoon <maheshpabeykoon@gmail.com>
2022-07-13 11:53:53 +0530

    Attach controller layer to service layer

--- src/main/java/com/sprigboot/school/controller/SchoolController.java
+++ src/main/java/com/sprigboot/school/controller/SchoolController.java
@@ -1,5 +1,25 @@
 package com.sprigboot.school.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sprigboot.school.service.SchoolService;
+
+@Controller
+@RequestMapping("api/students")
+
 public class SchoolController {
 
+	private SchoolService schoolService;
+	
+	SchoolController(SchoolService schoolService){
+		this.schoolService=schoolService;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
